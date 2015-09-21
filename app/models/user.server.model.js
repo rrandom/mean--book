ec7a423bd9f46004af69b3f05cd162c7e9b38ea7
Rrandom <emanonhere@gmail.com>
2015-09-21 22:10:20 +0800

    model validation and so on..

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,13 +4,55 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
   firstName: String,
   lastName: String,
-  email: String,
-  username: String,
-  password: String,
+  email: {
+    type: String,
+    index: true,
+    match: /.+@.+\..+/
+  },
+  username: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    validate: [
+      function(password) {
+        return password.length >= 6;
+      },
+      'Password should be longer'
+    ]
+  },
   created: {
     type: Date,
     default: Date.now
-  }
+  },
+  website: {
+    type: String,
+    get: function (url) {
+      if (!url) {
+        return url;
+      } else {
+        if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+          url = 'http://' + url;
+        }
+        return url;
+      }
+    }
+  },
+});
+
+UserSchema.virtual('fullName').get(function () {
+  return this.firstName + ' ' + this.lastName;
+}).set(function (fullName) {
+  var splitName = fullName.split(' ');
+  this.firstName = splitName[0] || '';
+  this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', {
+  getters: true
 });
 
 mongoose.model('User', UserSchema);
