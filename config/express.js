91fe72d1c76286885ade9fe1259cbf00405b46c6
Rrandom <emanonhere@gmail.com>
2015-09-26 16:37:31 +0800

    socketio integration

--- config/express.js
+++ config/express.js
@@ -1,15 +1,20 @@
 var config = require('./config'),
+  http = require('http'),
+  socketio = require('socket.io'),
   express = require('express'),
   morgan = require('morgan'),
   compress = require('compression'),
   bodyParser = require('body-parser'),
   methodOverride = require('method-override'),
   session = require('express-session'),
+  MongoStore = require('connect-mongo')(session),
   flash = require('connect-flash'),
   passport = require('passport');
 
-module.exports = function () {
+module.exports = function (db) {
   var app = express();
+  var server = http.createServer(app);
+  var io = socketio.listen(server);
 
   if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -23,10 +28,15 @@ module.exports = function () {
   app.use(bodyParser.json());
   app.use(methodOverride());
 
+  var mongoStore = new MongoStore({
+    db: db.connection.db
+  });
+
   app.use(session({
     saveUninitialized: true,
     resave: true,
-    secret: config.sessionSecret
+    secret: config.sessionSecret,
+    store: mongoStore
   }));
 
   app.set('views', './app/views');
@@ -43,5 +53,7 @@ module.exports = function () {
   // place this line in the end because the order matters
   app.use(express.static('./public'));
 
-  return app;
+  require('./socketio')(server, io, mongoStore);
+
+  return server;
 };
